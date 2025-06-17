7e98c547b05285e9b0552dd4479318a2ff52fa1d
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-17 13:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -44,4 +44,7 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+// alert(minDate(dates));
+console.log(minDate(["2023/03/01", "2023/03/02", "2023/03/03"])); // "2023/03/01"
+console.log(minDate(["2023/01/01", "2023/02/02", "2022/12/31"])); // "2022/12/31"
+
